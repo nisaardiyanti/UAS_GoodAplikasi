d2f2620c49eb3ce53e48bbff8a41be788f6a9f3d
nisaardiyanti <100394858+nisaardiyanti@users.noreply.github.com>
2022-12-24 12:37:55 +0700

    Update RemoteJobDao.kt

--- app/src/main/java/com/goodaplikasi/nisaardiyanti/db/RemoteJobDao.kt
+++ app/src/main/java/com/goodaplikasi/nisaardiyanti/db/RemoteJobDao.kt
@@ -7,13 +7,17 @@ import com.goodaplikasi.nisaardiyanti.models.JobToSave
 
 @Dao
 interface RemoteJobDao {
-
+//interface RemoteJobDao
+//no.8 Dao(query) terdapat pada baris 12-22
     @Insert(onConflict = OnConflictStrategy.REPLACE)
+     //Insert digunakan untuk menambahkan
     suspend fun insertJob(job: JobToSave): Long
 
     @Query("SELECT * FROM job ORDER BY id DESC")
+        //Select digunakan untuk menampilkan dari tabel job dengan order by
     fun getAllJob(): LiveData<List<JobToSave>>
 
     @Delete
+        //fungsi delete
     suspend fun deleteJob(job: JobToSave)
 }
