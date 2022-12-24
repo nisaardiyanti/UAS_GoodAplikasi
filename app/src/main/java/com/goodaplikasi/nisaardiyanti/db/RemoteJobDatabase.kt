73624cb0a4eaa4f16fb974406f8573584839cbdf
nisaardiyanti <100394858+nisaardiyanti@users.noreply.github.com>
2022-12-24 12:31:11 +0700

    Update RemoteJobDatabase.kt

--- app/src/main/java/com/goodaplikasi/nisaardiyanti/db/RemoteJobDatabase.kt
+++ app/src/main/java/com/goodaplikasi/nisaardiyanti/db/RemoteJobDatabase.kt
@@ -7,11 +7,16 @@ import androidx.room.RoomDatabase
 import com.goodaplikasi.nisaardiyanti.models.JobToSave
 
 @Database(entities = [JobToSave::class], version = 1)
+//no.9 entity(field) dari baris 9-28
 abstract class RemoteJobDatabase : RoomDatabase() {
-
+//kelas abstrak RemoteJobDatabase
     abstract fun getRemoteJobDao(): RemoteJobDao
-
+//function untuk mengambil RemoteJobJobDao
     companion object {
+//objek yang dideklarasikan dalam file yang sama dengan class , dan memiliki nama yang sama dengan class.
+    // Objek pendamping dan kelasnya dapat mengakses anggota pribadi satu sama lain.
+    // Metode terapkan objek pengiring memungkinkan Anda membuat instance baru dari kelas tanpa
+    // menggunakan kata kunci baru.    
         @Volatile
         private var instance: RemoteJobDatabase? = null
         private val LOCK = Any()
@@ -21,7 +26,9 @@ abstract class RemoteJobDatabase : RoomDatabase() {
         }
 
         private fun createDatabase(context: Context) =
+ //private function membuat database remoteJob_db2
             Room.databaseBuilder(
+//no.7 RoomDb(Konfigurasi db) terdapat pada baris 30-35            
                 context.applicationContext,
                 RemoteJobDatabase::class.java,
                 "remoteJob_db2"
