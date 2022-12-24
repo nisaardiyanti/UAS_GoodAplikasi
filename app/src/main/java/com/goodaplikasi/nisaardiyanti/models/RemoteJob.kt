56a6a8c162ad42759c0d51525f0235ea0e5cf8d5
nisaardiyanti <100394858+nisaardiyanti@users.noreply.github.com>
2022-12-24 11:59:33 +0700

    Update RemoteJob.kt

--- app/src/main/java/com/goodaplikasi/nisaardiyanti/models/RemoteJob.kt
+++ app/src/main/java/com/goodaplikasi/nisaardiyanti/models/RemoteJob.kt
@@ -3,6 +3,7 @@ package com.goodaplikasi.nisaardiyanti.models
 import com.google.gson.annotations.SerializedName
 
 data class RemoteJob(
+//4.class data(objek data) pada baris 5-10
         @SerializedName("job-count")
         val jobCount: Int?,
         val jobs: List<Job>?,
