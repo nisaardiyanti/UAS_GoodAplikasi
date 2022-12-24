167d825e3ca3638fceb8cff07a79935169514db7
nisaardiyanti <100394858+nisaardiyanti@users.noreply.github.com>
2022-12-24 11:46:33 +0700

    Update RemoteJobViewModel.kt

--- app/src/main/java/com/goodaplikasi/nisaardiyanti/viewmodel/RemoteJobViewModel.kt
+++ app/src/main/java/com/goodaplikasi/nisaardiyanti/viewmodel/RemoteJobViewModel.kt
@@ -14,21 +14,29 @@ class RemoteJobViewModel(
 ) : AndroidViewModel(app) {
 
     fun remoteJobResult() =
+    //No.2 ViewModel (live data) terdapat pada baris baris 18-25
         remoteJobRepository.getRemoteJobResponseLiveData()
-
+//mengambil RemoteJobResponse menggunakan livedata untuk mengambil data dari RemoteJobRepository
+//Fitur View (data-binding) ada di class RemoteJobViewModel  baris kode 18-25
     fun searchJob(query: String?) =
         remoteJobRepository.searchRemoteJob(query)
-
+//function cari menggunakan query string pada RemoteJobRepository
     fun searchResult() = remoteJobRepository.getSearchJobResponseLiveData()
+//function getSearchJobResponseLiveData untuk mengambil data dari remoteJobRepository
 
     fun insertJob(job: JobToSave) = viewModelScope.launch {
+            //function untuk memasukkan job
+
         remoteJobRepository.insertJob(job)
     }
 
     fun deleteJob(job: JobToSave) = viewModelScope.launch {
         remoteJobRepository.deleteJob(job)
+  //function deleteJob untuk deleteJob
+
     }
 
     fun getAllJob() = remoteJobRepository.getAllJobs()
+    //function mengambil data Job
 
 }
