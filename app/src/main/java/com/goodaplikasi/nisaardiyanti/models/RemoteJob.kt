863828d3171b67bafd2f358d6fcd72131dab440b
nisaardiyanti <100394858+nisaardiyanti@users.noreply.github.com>
2022-12-29 07:10:57 +0700

    Update RemoteJob.kt

--- app/src/main/java/com/goodaplikasi/nisaardiyanti/models/RemoteJob.kt
+++ app/src/main/java/com/goodaplikasi/nisaardiyanti/models/RemoteJob.kt
@@ -1,7 +1,7 @@
 package com.goodaplikasi.nisaardiyanti.models
 
 import com.google.gson.annotations.SerializedName
-
+//todo 5: kelas model yang berfungsi untuk merepresentasikan data yang akan diproses dan menentukan properti dan atribut kelas model
 data class RemoteJob(
 //4.class data(objek data) pada baris 5-10
         @SerializedName("job-count")
