47fe5e9253bd5205ea37c13993e53e585eb88773
nisaardiyanti <100394858+nisaardiyanti@users.noreply.github.com>
2023-01-03 03:40:53 +0700

    Update RemoteJobAPI.kt

--- app/src/main/java/com/goodaplikasi/nisaardiyanti/api/RemoteJobAPI.kt
+++ app/src/main/java/com/goodaplikasi/nisaardiyanti/api/RemoteJobAPI.kt
@@ -6,12 +6,11 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 //Todo 1 : bagian retrofit yang akan mengambil data dari API
-
+//bagian retrofit yang akan mengambil data repository pada class RemoteJobAPI.kt pada baris ke 10-20
 interface RemoteJobAPI {
     //no.5 network api (parsing data ) terdapat pada baris 14-19
     //Mengirim permintaan ke API dan interface sebagai tampilan atau antarmuka yang tujuannya,agar pengguna dapat berinteraksi
-// atau menggunakan perangkatnya digital secara langsung maupun melalui jaringan
-    
+// atau menggunakan perangkatnya digital secara langsung maupun melalui jaringan   
     @GET("remote-jobs")
 //menggunakan fungsi get
     fun getRemoteJob(): Call<RemoteJob>
